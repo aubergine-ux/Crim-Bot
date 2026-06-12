d70918de67401b9ad2d9ae9037951e1d6b37cd8b
aubergine_ux <126033183+aubergine-ux@users.noreply.github.com>
2026-06-12 05:43:14 -0400

    v26.5 | 3 New Commands

---
+++ commands/moderation/warn.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    // 1. This sets up how the command looks inside Discord
+    data: new SlashCommandBuilder()
+        .setName('warn') // Must be lowercase, no spaces
+        .setDescription('Sends a Server User a Warning')
+        .addUserOption(option =>
+            option.setName('target').setDescription('The User to be Warned.').setRequired(true))
+        .addStringOption(option =>
+            option.setName('reason').setDescription('The Reason for the Warning').setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+    
+    // 2. This is what happens when someone types the command
+    async execute(interaction) {
+
+        const user = interaction.options.getUser('target');
+        const reason = interaction.options.getString('reason');
+
+        try {
+            await user.send(`You have been warned in **${interaction.guild.name}** for: ${reason}`);
+            await interaction.reply({ content: `User **${user.tag}** Has Been Warned` });
+        } catch (error) {
+            // Send a message back to the user
+            await interaction.reply({ content: `Could not DM **${user.tag}** (DMs turned off), but the warning has been logged ${reason}` });
+        }
+    },
+};
