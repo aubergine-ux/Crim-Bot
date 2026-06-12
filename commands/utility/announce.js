67c6f0e744cf1775d2c70e0911863430c54bc89a
aubergine_ux <126033183+aubergine-ux@users.noreply.github.com>
2026-06-12 07:26:58 -0400

    CB v26.11 | Fixed Announce

--- commands/utility/announce.js
+++ commands/utility/announce.js
@@ -1,27 +1,44 @@
-const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, ChannelType, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
-    // 1. This sets up how the command looks inside Discord
     data: new SlashCommandBuilder()
-        .setName('announce') // Must be lowercase, no spaces
-        .setDescription('Sends a Embedded Announcement in the Desired Channel')
-        .addStringOption(option =>
-            option.setName('text')
-                .setDescription('Message Contents')
-                .setRequired(true))
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
-
-    // 2. This is what happens when someone types the command
+        .setName('announce')
+        .setDescription('Sends an Announcement in the Desired Channel')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+        .addStringOption((option) =>
+            option
+                .setName('input')
+                .setDescription('The input to announce back')
+                .setRequired(true)
+                .setMaxLength(2_000),
+        )
+        .addChannelOption((option) =>
+            option
+                .setName('channel')
+                .setDescription('The channel to announce into')
+                .addChannelTypes(ChannelType.GuildText),
+        )
+        .addBooleanOption((option) =>
+            option
+                .setName('embed')
+                .setDescription('Whether or not the announcement should be embedded'),
+        ),
     async execute(interaction) {
-        
-        const text = interaction.options.getString('text');
+        const input = interaction.options.getString('input');
+        const channel = interaction.options.getChannel('channel') ?? interaction.channel;
+        const embed = interaction.options.getBoolean('embed') ?? false;
 
-        const embed = new EmbedBuilder()
-            .setTitle ('Server Announcement')
-            .setDescription(text)
-            .setColor('Blurple')
-            .setTimestamp();
-        // Send a message back to the user
-        await interaction.reply({ embeds: [embed] });
+        if (embed) {
+            await channel.send({ embeds: [{ description: input }] });
+        } else {
+            await channel.send(input);
+        }
+
+        // Acknowledge the interaction if echoing to a different channel
+        if (channel.id !== interaction.channel.id) {
+            await interaction.reply({ content: `Message sent to ${channel}!`, ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'Done!', ephemeral: true });
+        }
     },
-};
+}
